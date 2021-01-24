a425dc67431cd667d80796d8297186c38bed08c5
DionisPalpatin <77577723+DionisPalpatin@users.noreply.github.com>
2021-01-24 16:47:11 +0300

    Add files via upload

---
+++ Ellipse.py
@@ -0,0 +1,26 @@
+import pygame as pg
+from math import sqrt, ceil
+
+size = 800
+try:
+    n = int(input())
+    step = size // n // 2
+
+    pg.init()
+    window = pg.display.set_mode((size, size), pg.SHOWN)
+    window.fill((0, 0, 0))
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                exit()
+
+        for i in range(1, n + 1):
+            pg.draw.ellipse(window, (255, 255, 255), (size // 2 - step * i, 0, 2 * step * i, size), 1)
+            pg.draw.ellipse(window, (255, 255, 255), (0, size // 2 - step * i, size, 2 * step * i), 1)
+            pg.display.update()
+
+        pg.time.delay(10)
+except:
+    print("Unsupported input format")
