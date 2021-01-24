11b2aa3afa16813bad2e034d3734d0c760e81c4d
DionisPalpatin <77577723+DionisPalpatin@users.noreply.github.com>
2021-01-24 16:50:48 +0300

    Update Ellipse.py

--- Ellipse.py
+++ Ellipse.py
@@ -15,12 +15,16 @@ try:
             if event.type == pg.QUIT:
                 pg.quit()
                 exit()
-
+                
+                
+        pg.draw.circle(window, (255, 255, 255), (size // 2, size // 2), step, 1)
         for i in range(1, n + 1):
             pg.draw.ellipse(window, (255, 255, 255), (size // 2 - step * i, 0, 2 * step * i, size), 1)
             pg.draw.ellipse(window, (255, 255, 255), (0, size // 2 - step * i, size, 2 * step * i), 1)
-            pg.display.update()
-
+            
+            
+        pg.display.update()
         pg.time.delay(10)
+        
 except:
     print("Unsupported input format")
